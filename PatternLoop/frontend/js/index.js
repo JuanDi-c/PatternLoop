58ccfc3a9e8c01921be63f13718b72ec58729ac9
Laura Sofia <laumart.c09@gmail.com>
2026-07-17 15:43:24 -0500

    pub_recientes

--- PatternLoop/frontend/js/index.js
+++ PatternLoop/frontend/js/index.js
@@ -8,3 +8,90 @@ if (sessionStorage.getItem("logoutExitoso")) {
     sessionStorage.removeItem("logoutExitoso");
 
 }
+
+const grid = document.getElementById("patternsGrid");
+console.log(grid);
+
+cargarPublicaciones();
+
+async function cargarPublicaciones(){
+
+    try{
+
+        const respuesta = await fetch(`${API_URL}/publicaciones`);
+
+        const publicaciones = await respuesta.json();
+
+        mostrarPublicaciones(publicaciones);
+
+    }catch(error){
+
+        console.error(error);
+
+    }
+
+}
+
+function mostrarPublicaciones(publicaciones){
+
+    grid.innerHTML = "";
+
+    publicaciones.forEach(publicacion=>{
+
+        const imagen = publicacion.imagenes.length
+    ? `${API_URL.replace("/api","")}/uploads/${publicacion.imagenes[0]}`
+    : "images/no-image.png";
+
+        grid.innerHTML += `
+
+            <article class="pattern-card">
+
+                <img
+                    src="${imagen}"
+                    alt="${publicacion.titulo}">
+
+                <div class="pattern-info">
+
+                    <span class="category">
+
+                        ${publicacion.categoria}
+
+                    </span>
+
+                    <h3>
+
+                        ${publicacion.titulo}
+
+                    </h3>
+
+                    <p>
+
+                        ${publicacion.descripcion.substring(0,90)}...
+
+                    </p>
+
+                    <div class="pattern-footer">
+
+                        <span>
+
+                            👤 ${publicacion.autor.nombre}
+
+                        </span>
+
+                        <span>
+
+                            ❤️ ${publicacion.likes}
+
+                        </span>
+
+                    </div>
+
+                </div>
+
+            </article>
+
+        `;
+
+    });
+
+}
